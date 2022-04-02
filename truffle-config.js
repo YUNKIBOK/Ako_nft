0cd9b3c260e672b9782e680d2854774ebc36d6b5
smileyr909 <smileyr909@gmail.com>
2022-04-03 01:03:46 +0900

    initial commit

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "5777" // Match any network id
     },
   },
@@ -17,6 +17,6 @@ module.exports = {
         enabled: true,
         runs: 200
       }
-    }
+    } 
   }
 }
